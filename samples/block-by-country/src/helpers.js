5afd4a1843e5a677d3539cb0231bcee61d046aa4
Michelle Dhanani <michelle@fermyon.com>
2025-02-18 18:02:02 -0500

    update ipv4 regexes
    Fixes ipv4 regex to match port appropriately.
    
    Signed-off-by: Michelle Dhanani <michelle@fermyon.com>
    Co-signed-by: Brian Harcdock <brian.hardock@fermyon.com>

--- samples/block-by-country/src/helpers.js
+++ samples/block-by-country/src/helpers.js
@@ -7,13 +7,16 @@ const getClientAddressFromRequest = (req) => {
 };
 
 const cleanupIpAddress = (input) => {
-  const ipv4Regex = /^(\d{1,3}\.){3}\d{1,3}:/;
+  const ipv4Regex = /^(\d{1,3}\.){3}\d{1,3}$/;
+  const ipv4RegexWithPort = /^(\d{1,3}\.){3}\d{1,3}:\d+$/;
   const ipv6WithPortRegex = /^\[([a-fA-F0-9:]+)\]:\d+$/;
 
-  if (ipv4Regex.test(input)) {
+  if (ipv4RegexWithPort.test(input)) {
     return input.split(':')[0];
   } else if (ipv6WithPortRegex.test(input)) {
     return input.match(ipv6WithPortRegex)[1];
+  } else if (ipv4Regex.test(input)) {
+    return input;
   } else {
     return input;
   }
